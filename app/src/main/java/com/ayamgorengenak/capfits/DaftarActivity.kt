c7caed171813ab264efe8c1e5d046a84e3deb6fb
anekoinda <shiltainda@gmail.com>
2022-05-14 16:58:53 +0800

    daftar

--- app/src/main/java/com/ayamgorengenak/capfits/DaftarActivity.kt
+++ app/src/main/java/com/ayamgorengenak/capfits/DaftarActivity.kt
@@ -3,17 +3,18 @@ package com.ayamgorengenak.capfits
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ayamgorengenak.capfits.databinding.ActivityDaftarBinding
 import com.ayamgorengenak.capfits.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+class DaftarActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDaftarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityDaftarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonDaftar.setOnClickListener {
-            Intent(this@LoginActivity, DaftarActivity::class.java).also {
+        binding.buttonLogin.setOnClickListener {
+            Intent(this@DaftarActivity, LoginActivity::class.java).also {
                 startActivity(it)
             }
         }
