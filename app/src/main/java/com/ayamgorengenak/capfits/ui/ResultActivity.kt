2321bfb38eb614c0195e0784cc9b6448c541543f
anekoinda <shiltainda@gmail.com>
2022-05-27 02:07:10 +0800

    bottom sheet

--- app/src/main/java/com/ayamgorengenak/capfits/ui/ResultActivity.kt
+++ app/src/main/java/com/ayamgorengenak/capfits/ui/ResultActivity.kt
@@ -3,10 +3,20 @@ package com.ayamgorengenak.capfits.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ayamgorengenak.capfits.R
+import com.ayamgorengenak.capfits.databinding.ActivityLoginBinding
+import com.ayamgorengenak.capfits.databinding.ActivityResultBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        BottomSheetBehavior.from(binding.sheet).apply {
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
     }
 }
