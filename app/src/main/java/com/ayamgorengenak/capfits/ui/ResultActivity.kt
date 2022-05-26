da804740eb69326553d18ba0c33dbdfd207f138b
malaupase47 <adampase9@gmail.com>
2022-05-27 00:38:04 +0800

    category

---
+++ app/src/main/java/com/ayamgorengenak/capfits/ui/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.ayamgorengenak.capfits.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ayamgorengenak.capfits.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
