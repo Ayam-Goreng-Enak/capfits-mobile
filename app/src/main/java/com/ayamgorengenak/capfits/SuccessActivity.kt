cd984bfb940bfcfebd75bb4601df14be4a146db7
anekoinda <shiltainda@gmail.com>
2022-06-11 12:29:44 +0800

    back home button

--- app/src/main/java/com/ayamgorengenak/capfits/SuccessActivity.kt
+++ app/src/main/java/com/ayamgorengenak/capfits/SuccessActivity.kt
@@ -1,11 +1,24 @@
 package com.ayamgorengenak.capfits
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ayamgorengenak.capfits.databinding.ActivityDetailProductBinding
+import com.ayamgorengenak.capfits.databinding.ActivitySuccessBinding
+import com.ayamgorengenak.capfits.ui.home.HomeActivity
 
+private lateinit var binding: ActivitySuccessBinding
 class SuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_success)
+
+        binding = ActivitySuccessBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonHome.setOnClickListener {
+            Intent(this@SuccessActivity, HomeActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
